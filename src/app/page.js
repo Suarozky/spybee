05c1300a5d8aaffeb1e8a13915956759b83eb3ed
Brayansimp <Brayansuarez2809@gmail.com>
2025-02-07 15:46:33 -0500

    <FIX>

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
 import FilterInput from "../components/filterInput/filterInput";
-import Projects from "../components/project/project";
-import { getData } from "../lib/data";  // Importa correctamente
+
 
 export default async function Home() {
 
