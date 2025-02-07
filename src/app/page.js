af5d4f7678e000cfceb3b9c7d977fc74251ef20c
Brayansimp <Brayansuarez2809@gmail.com>
2025-02-07 15:49:56 -0500

    <FEAT>despliegue

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
-
+import Image from "next/image";
+import styles from "./page.module.css";
+import FilterInput from "../components/filterInput/filterInput";
+import Projects from "../components/project/project";
+import { getData } from "../lib/data";  // Importa correctamente
 
 export default async function Home() {
-
-
+  const data = await getData();
+  console.log(data);
   return (
     <div>
-  
-
+      <FilterInput />
+      <Projects inicialData={data} />
     </div>
   );
 }
